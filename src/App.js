b1de12bdb12bdf73db9a0c1dc7ca01d4d5eac173
Animesh Maiti <a.maiti770@gmail.com>
2023-05-27 13:47:56 +0530

    created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Follower from './components/Follower';
+import Header from './components/Header';
+import Overview from './components/Overview';
+
+function App() {
+  return (
+    <>
+      <section>
+        <div/>
+        <Header/>
+        <Follower/>
+        <Overview/>
+      </section>
+    </>
+  );
+}
+
+export default App;
