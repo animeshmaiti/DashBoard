92ad6cee6ecaf727d33042d7bc8c88cc682ddd94
Animesh Maiti <a.maiti770@gmail.com>
2023-06-15 17:46:51 +0530

    section height issue solved by chatGpt

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Overview from './components/Overview';
 function App() {
   return (
     <>
-      <section className='p-8 dark:bg-slate-900 h-full'>
-        <div 
+      <section className='p-8 dark:bg-slate-900 min-h-screen'>
+        <div
           className='bg-slate-100 dark:bg-slate-800 h-52 w-full absolute top-0 left-0 rounded-b-3xl'
-          style={{zIndex: 0}}
+          style={{ zIndex: 0 }}
         ></div>
-        <Header/>
-        <Follower/>
-        <Overview/>
+        <Header />
+        <Follower />
+        <Overview />
       </section>
     </>
   );
